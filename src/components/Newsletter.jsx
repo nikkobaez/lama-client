6d8065c9c042a9b4b7223d2303905906766911a0
Nikko Baez <Nikkobaez82@gmail.com>
2023-12-26 23:02:01 -0600

    mib

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -3,10 +3,10 @@ import { IoSend } from "react-icons/io5";
 const Newsletter = () => {
     return (
         <div className='flex h-[60vh] flex-col items-center justify-center bg-[#f8e1d8]'>
-            <h1 className='mb-5 text-5xl'>
+            <h1 className='mb-5 text-4xl md:text-5xl'>
                 Newsletter
             </h1>
-            <p className='mb-5 text-xl lg:text-2xl font-extralight'>
+            <p className='mb-5 text-center text-md md:text-2xl font-extralight'>
                 Get timely updates from your favorite products
             </p>
             <div className="flex justify-between w-1/2 h-10 max-w-md bg-white border border-gray-300">
